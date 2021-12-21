refactorisation, correct (?) covariance computation

src/filters.py
@@ -8,7 +8,7 @@ def guided_filter(im_P: np.ndarray, im_I: np.ndarray, r: int, eps: float) -> np.
 
     if len(im_P.shape) == 3:
         return np.stack([guided_filter(channel_P, im_I, r, eps)
-                         for channel_P in np.transpose(im_P, 2, 0, 1)], axis=-1)
+                         for channel_P in np.transpose(im_P, (2, 0, 1))], axis=-1)
 
     if len(im_I.shape) == 3:
         return guided_filter_rgb(im_P, im_I, r, eps)