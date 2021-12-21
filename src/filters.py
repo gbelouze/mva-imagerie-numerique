4a427880edc3ac72c8d51d8be7f3823195645bce
r__r <rpl159@laposte.net>
2021-12-21 17:13:09 +0100

    gf: use scipy uniform_filter

--- src/filters.py
+++ src/filters.py
@@ -1,10 +1,5 @@
 import numpy as np
-from scipy.ndimage import convolve
-
-
-def mean_kernel(size: int, d: int) -> np.ndarray:
-    shape = [size, size] + [1] * (d - 2)
-    return np.ones((size, size))[None, :].reshape(shape) / size ** 2
+from scipy.ndimage import uniform_filter
 
 
 def guided_filter(im_P: np.ndarray, im_I: np.ndarray, r: int, eps: float) -> np.ndarray:
@@ -12,18 +7,18 @@ def guided_filter(im_P: np.ndarray, im_I: np.ndarray, r: int, eps: float) -> np.
         return np.stack([guided_filter(ch, im_I, r, eps)
                          for ch in np.moveaxis(im_P, -1, 0)], axis=-1)
 
-    k_mean = mean_kernel(2 * r + 1, 2)
+    size = 2 * r + 1, 2
 
-    mean_I = convolve(im_I, k_mean)
-    mean_P = convolve(im_P, k_mean)
-    mean_IP = convolve(im_I * im_P, k_mean)
-    var_I = convolve(im_I ** 2, k_mean) - mean_I ** 2
+    mean_I = uniform_filter(im_I, size)
+    mean_P = uniform_filter(im_P, size)
+    mean_IP = uniform_filter(im_I * im_P, size)
+    var_I = uniform_filter(im_I ** 2, size) - mean_I ** 2
 
     a = (mean_IP - mean_I * mean_P) / (var_I + eps)
     b = mean_P - a * mean_I
 
-    a = convolve(a, k_mean)
-    b = convolve(b, k_mean)
+    a = uniform_filter(a, size)
+    b = uniform_filter(b, size)
     return a * im_I + b
 
 
@@ -34,15 +29,12 @@ def guided_filter_rgb(im_P: np.ndarray, im_I: np.ndarray, r: int, eps: float) ->
     m, n = im_P.shape
     size = 2 * r + 1
 
-    k_mean = mean_kernel(size, 2)
-    k_mean_3d = mean_kernel(size, 3)
+    mean_I = uniform_filter(im_I, (size, size, 0))
+    mean_P = uniform_filter(im_P, size)
+    mean_IP = uniform_filter(im_I * im_P[None, :].reshape(m, n, 1), (size, size, 0))
 
-    mean_I = convolve(im_I, k_mean_3d)
-    mean_P = convolve(im_P, k_mean)
-    mean_IP = convolve(im_I * im_P[None, :].reshape(m, n, 1), k_mean_3d)
-
-    sig = convolve(np.einsum('ijk,ijl->ijkl', im_I, im_I),
-                   mean_kernel(size, 4)) * (size ** 2 / (size ** 2 - 1))
+    sig = uniform_filter(np.einsum('ijk,ijl->ijkl', im_I, im_I),
+                         (size, size, 0, 0)) * (size ** 2 / (size ** 2 - 1))
 
     a = np.einsum('ijkl,ijl->ijk',
                   sig + eps * np.identity(3),
@@ -50,7 +42,7 @@ def guided_filter_rgb(im_P: np.ndarray, im_I: np.ndarray, r: int, eps: float) ->
 
     b = mean_P - np.einsum('ijk,ijk->ij', a, mean_I)
 
-    a = convolve(a, k_mean_3d)
-    b = convolve(b, k_mean)
+    a = uniform_filter(a, (size, size, 0))
+    b = uniform_filter(b, size)
 
     return np.einsum('ijk,ijk->ij', a, im_I) + b
