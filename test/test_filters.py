70bf4b597fc3b95fbf8d4a5df2b1d405e0f2ff62
Gabriel Belouze <gabriel.belouze@ens.psl.eu>
2021-12-23 11:36:33 +0100

    add some tests

---
+++ test/test_filters.py
@@ -0,0 +1,24 @@
+from gf import filters
+import numpy as np
+from imageio import imread
+import os
+
+
+def test_anisotropy_rgb_guide():
+    """filters should be covariant with pi/2 rotations"""
+    im = imread(f"{os.path.dirname(__file__)}/../data/lytro/lytro-01-A.jpg") / 255
+    im_t = np.transpose(im, (1, 0, 2))
+    out = filters.guided_filter(im, im, 10, 0.1)
+    out_t = filters.guided_filter(im_t, im_t, 10, 0.1)
+    diff = np.abs(out - np.transpose(out_t, (1, 0, 2))).max()
+    assert diff < 1e-6
+
+
+def test_anisotropy_gray_guide():
+    """filters should be covariant with pi/2 rotations"""
+    im = imread(f"{os.path.dirname(__file__)}/../data/petrovic/fused001_1.tif") / 255
+    im_t = np.transpose(im, (1, 0))
+    out = filters.guided_filter(im, im, 10, 0.1)
+    out_t = filters.guided_filter(im_t, im_t, 10, 0.1)
+    diff = np.abs(out - np.transpose(out_t, (1, 0))).max()
+    assert diff < 1e-6
---
+++ test/test_fusion.py
@@ -0,0 +1,32 @@
+from gf import fusion
+import numpy as np
+from imageio import imread
+import os
+
+im_gray = imread(f"{os.path.dirname(__file__)}/../data/petrovic/fused001_1.tif") / 255
+im_rgb = imread(f"{os.path.dirname(__file__)}/../data/lytro/lytro-01-A.jpg") / 255
+
+
+def test_decompose_gray():
+    b, d = fusion.decompose(im_gray)
+    diff = np.abs(im_gray - (b + d)).max()
+    assert diff < 1e-10
+
+
+def test_decompose_rgb():
+    b, d = fusion.decompose(im_rgb)
+    diff = np.abs(im_rgb - (b + d)).max()
+    assert diff < 1e-10
+
+
+def test_fusion_black_black():
+    black = np.zeros((100, 100))
+    fused = fusion.fusion([black, black])
+    diff = np.abs(fused - black).max()
+    assert diff < 1e-10
+
+
+def test_fusion_black_white():
+    black = np.zeros((100, 100))
+    white = np.ones((100, 100))
+    fusion.fusion([black, white])
