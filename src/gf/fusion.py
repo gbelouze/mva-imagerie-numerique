fbff0529fa063871cbae2972aa9836e529ad97d7
Gabriel Belouze <gabriel.belouze@ens.psl.eu>
2022-02-06 13:36:52 +0100

    store intermediate variables of the image fusion

--- src/gf/fusion.py
+++ src/gf/fusion.py
@@ -8,6 +8,7 @@ from scipy.ndimage import uniform_filter, laplace, gaussian_filter  # type: igno
 def decompose(
     im: np.ndarray, multichannel: Optional[bool] = None, k: int = 31
 ) -> Tuple[np.ndarray, np.ndarray]:
+    """base / detail layer decomposition"""
     if multichannel is None:
         multichannel = im.ndim == 3
     size = [k, k, 0] if multichannel else k
@@ -28,6 +29,11 @@ class gff:
         self.color = color
         self.multichannel = color != "gray"
 
+        self.compute_weight_maps()
+        self.refined_weights = {"base": np.empty(0), "detail": np.empty(0)}
+        self.normalised_refined_weights = {"base": np.empty(0), "detail": np.empty(0)}
+        self.fused = {"base": np.empty(0), "detail": np.empty(0)}
+
     def decompose(self, im: np.ndarray, k: int = 31) -> Tuple[np.ndarray, np.ndarray]:
         return decompose(im, self.multichannel, k)
 
@@ -40,25 +46,32 @@ class gff:
         s = gaussian_filter(np.abs(h), sigma, truncate=r / sigma)
         return s
 
-    def weight_maps(self, ims: List[np.ndarray]) -> List[np.ndarray]:
-        saliencies = np.stack([self.saliency(im) for im in ims], axis=0)
-        weights = (saliencies == np.max(saliencies, axis=0)[None, :]).astype(np.float64)
-        return [w for w in weights]
+    def compute_weight_maps(self):
+        self.saliencies = np.stack([self.saliency(im) for im in self.ims], axis=0)
+        self.weights = (self.saliencies == np.max(self.saliencies, axis=0)[None, :]).astype(
+            np.float64
+        )
 
     def fusion(self, r1=45, r2=7, eps1=0.3, eps2=1e-6, filt=guided_filter):
         bs, ds = zip(*[self.decompose(im) for im in self.ims])
-        weights = self.weight_maps(self.ims)
+        weights = [w for w in self.weights]
 
-        weights_b = np.stack([filt(p, i, r1, eps1) for p, i in zip(weights, self.ims)])
-        weights_d = np.stack([filt(p, i, r2, eps2) for p, i in zip(weights, self.ims)])
-        weights_b = weights_b / np.sum(weights_b, axis=0)
-        weights_d = weights_d / np.sum(weights_b, axis=0)
+        self.refined_weights["base"] = weights_b = np.stack(
+            [filt(p, i, r1, eps1) for p, i in zip(weights, self.ims)]
+        ).clip(0, 1)
+        self.refined_weights["detail"] = weights_d = np.stack(
+            [filt(p, i, r2, eps2) for p, i in zip(weights, self.ims)]
+        ).clip(0, 1)
+        self.normalised_refined_weights["base"] = weights_b = weights_b / np.sum(weights_b, axis=0)
+        self.normalised_refined_weights["detail"] = weights_d = weights_d / np.sum(
+            weights_b, axis=0
+        )
         if self.multichannel:
-            b_bar = sum(w[:, :, None] * b for w, b in zip(weights_b, bs))
-            d_bar = sum(w[:, :, None] * d for w, d in zip(weights_d, ds))
+            self.fused["base"] = b_bar = sum(w[:, :, None] * b for w, b in zip(weights_b, bs))
+            self.fused["detail"] = d_bar = sum(w[:, :, None] * d for w, d in zip(weights_d, ds))
         else:
-            b_bar = sum(w * b for w, b in zip(weights_b, bs))
-            d_bar = sum(w * d for w, d in zip(weights_d, ds))
+            self.fused["base"] = b_bar = sum(w * b for w, b in zip(weights_b, bs))
+            self.fused["detail"] = d_bar = sum(w * d for w, d in zip(weights_d, ds))
         out = np.clip(b_bar + d_bar, 0, 1)
         return out
 
