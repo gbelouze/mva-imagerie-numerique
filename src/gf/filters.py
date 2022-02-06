c332df8b6b95e41176c3ee01fb0482f04d112c6c
Gabriel Belouze <gabriel.belouze@ens.psl.eu>
2022-02-06 01:16:14 +0100

    use array.ndim

--- src/gf/filters.py
+++ src/gf/filters.py
@@ -2,25 +2,30 @@ import numpy as np
 from scipy.ndimage import uniform_filter  # type: ignore
 
 
-def guided_filter(im_P: np.ndarray, im_I: np.ndarray, r: int, eps: float) -> np.ndarray:
-    assert 2 <= len(im_P.shape) <= 3
-    assert 2 <= len(im_I.shape) <= 3
+def guided_filter(im_P: np.ndarray, im_I: np.ndarray, *args, **kwargs) -> np.ndarray:
+    assert 2 <= im_P.ndim <= 3
+    assert 2 <= im_I.ndim <= 3
 
-    if len(im_P.shape) == 3:
+    if im_P.ndim == 3:
         return np.stack(
-            [guided_filter(channel_P, im_I, r, eps) for channel_P in np.transpose(im_P, (2, 0, 1))],
+            [
+                guided_filter(channel_P, im_I, *args, **kwargs)
+                for channel_P in np.transpose(im_P, (2, 0, 1))
+            ],
             axis=-1,
         )
 
-    if len(im_I.shape) == 3:
-        return guided_filter_rgb(im_P, im_I, r, eps)
+    if im_I.ndim == 3:
+        return guided_filter_rgb(im_P, im_I, *args, **kwargs)
     else:
-        return guided_filter_gray(im_P, im_I, r, eps)
+        return guided_filter_gray(im_P, im_I, *args, **kwargs)
 
 
-def guided_filter_gray(im_P: np.ndarray, im_I: np.ndarray, r: int, eps: float) -> np.ndarray:
-    assert len(im_I.shape) == 2
-    assert len(im_P.shape) == 2
+def guided_filter_gray(
+    im_P: np.ndarray, im_I: np.ndarray, r: int, eps: float, average_in_window: bool = True
+) -> np.ndarray:
+    assert im_I.ndim == 2
+    assert im_P.ndim == 2
 
     size = 2 * r + 1
 
@@ -32,14 +37,17 @@ def guided_filter_gray(im_P: np.ndarray, im_I: np.ndarray, r: int, eps: float) -
     a = (mean_IP - mean_I * mean_P) / (var_I + eps)
     b = mean_P - a * mean_I
 
-    a = uniform_filter(a, size)
-    b = uniform_filter(b, size)
+    if average_in_window:
+        a = uniform_filter(a, size)
+        b = uniform_filter(b, size)
     return a * im_I + b
 
 
-def guided_filter_rgb(im_P: np.ndarray, im_I: np.ndarray, r: int, eps: float) -> np.ndarray:
-    assert len(im_I.shape) == 3
-    assert len(im_P.shape) == 2
+def guided_filter_rgb(
+    im_P: np.ndarray, im_I: np.ndarray, r: int, eps: float, average_in_window: bool = True
+) -> np.ndarray:
+    assert im_I.ndim == 3
+    assert im_P.ndim == 2
 
     size = 2 * r + 1
 
@@ -59,7 +67,8 @@ def guided_filter_rgb(im_P: np.ndarray, im_I: np.ndarray, r: int, eps: float) ->
     )
     b = mean_P - np.einsum("ijk,ijk->ij", a, mean_I)
 
-    a = uniform_filter(a, (size, size, 0))
-    b = uniform_filter(b, size)
+    if average_in_window:
+        a = uniform_filter(a, (size, size, 0))
+        b = uniform_filter(b, size)
 
     return np.einsum("ijk,ijk->ij", a, im_I) + b
